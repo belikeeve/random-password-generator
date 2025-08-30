1ac6fc0bac08f09710d45920fd186d7362e55632
belikeeve <hello@jasminenelson.pro>
2025-08-30 14:03:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-// This is a reusable function that will generate and return one random password with.a maximum length of 15
+// This is a reusable function that will generate and return one random password with a maximum length of 15
 function generateRandomPassword() {
   const passwordLength = 15;
   let password = ""; // At the start, there should be no passwords displayed
@@ -11,7 +11,7 @@ function generateRandomPassword() {
     const randomIndex = Math.floor(Math.random() * characters.length);
     password += characters[randomIndex];
   }
-  return password; // This sends or hands off the final password string back to the function that called it. Remember, password starts out as blank.
+  return password; // This sends the final password string back to the function that called it. 
 }
 
 // Get the HTML elements
@@ -28,7 +28,7 @@ function generateRandomPassword() {
   const firstPassword = generateRandomPassword();
   const secondPassword = generateRandomPassword();
   
-// Update the HTML elements after you have the final results
+// Update the HTML elements after the final results have been generated
   passwordOne.textContent = firstPassword;
   passwordTwo.textContent = secondPassword;
   
